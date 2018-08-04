50518a9b8d76653c59aa5e6f6b5db4774ae3a08e
Richard Goulter <richard.goulter@gmail.com>
2018-08-04 16:57:56 +0800

    Update test.sh

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -2,6 +2,7 @@
 
 UNITY_BIN="/Applications/Unity/Unity.app/Contents/MacOS/Unity"
 
+echo "Running playmode tests"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -force-free \
   -runTests \
@@ -10,3 +11,9 @@ UNITY_BIN="/Applications/Unity/Unity.app/Contents/MacOS/Unity"
   -logFile $(pwd)/tests_playmode.log \
   -testResults $(pwd)/results_playmode.xml \
   -testPlatform playmode
+
+echo "Logs of playmode tests":
+cat $(pwd)/tests_playmode.log
+
+echo "XML of playmode tests":
+cat $(pwd)/results_playmode.xml
